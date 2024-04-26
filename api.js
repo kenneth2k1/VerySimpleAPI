4b3bea5a8008e861a37a8263510e0b1449c0d7fc
Ken Villalpando <Ken.Villalpando@amnhealthcare.com>
2024-04-25 19:09:25 -0700

    modified employee names

--- api.js
+++ api.js
@@ -4,8 +4,8 @@ const port = 9999;
 
 const employees = [
   { empID: 1, name: 'Ken V' },
-  { empID: 2, name: 'Ken V'},
-  { empID: 3, name: 'Ken V' },
+  { empID: 2, name: 'Ken V is the best choice'},
+  { empID: 3, name: 'Ken V will work hard for you' },
 ];
 
 api.get('/api/employees', (req, res) => {
